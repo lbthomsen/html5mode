a01ec4c59ecacc52da1e2486685da4dc25787d04
Lars Boegild Thomsen <lth@netcompartner.com>
2016-07-15 16:43:46 +0800

    Cosmetic

--- app.js
+++ app.js
@@ -2,25 +2,25 @@
  * app.js
  */
 
-    var appName = 'app';
+var appName = 'app';
 
-    var app = angular.module(appName, [
+var app = angular.module(appName, [
         'ngRoute'
-    ]);
+]);
 
-    app.config(['$locationProvider', '$routeProvider', 
-        function ($locationProvider, $routeProvider) {
+app.config(['$locationProvider', '$routeProvider', 
+    function ($locationProvider, $routeProvider) {
 
-            $routeProvider
-                .when('/', {
-                    templateUrl: 'templates/problem.html'
-                })
-                .when('/solution', {
-                    templateUrl: 'templates/solution.html'
-                })
-                .when('/about', {
-                    templateUrl: 'templates/about.html'
-                });
+        $routeProvider
+            .when('/', {
+                templateUrl: 'templates/problem.html'
+            })
+            .when('/solution', {
+                templateUrl: 'templates/solution.html'
+            })
+            .when('/about', {
+                templateUrl: 'templates/about.html'
+            });
 
         // None of that hash bang crap
         $locationProvider.html5Mode({
